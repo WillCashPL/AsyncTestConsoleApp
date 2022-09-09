5105972cb8dca4e604a1e4aa8c631e8d55a1c651
Lukasz Klobucki <lukasz.klobucki@gmail.com>
2022-09-09 09:21:32 +0200

    Program itself

---
+++ AsyncTestConsoleApp/AsyncClass.cs
@@ -0,0 +1,11 @@
+﻿namespace AsyncTestConsoleApp;
+
+public class AsyncClass
+{
+    public static async Task WillSitAndWaitForSomeTime(int seconds, CancellationToken token)
+    {
+        Console.WriteLine($"[Thread: {Thread.CurrentThread.ManagedThreadId}] Async method will wait for: {seconds} s");
+        await Task.Delay(seconds*1000,token);
+        Console.WriteLine($"[Thread: {Thread.CurrentThread.ManagedThreadId}] Done, let's go.");
+    }
+}
---
+++ AsyncTestConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using AsyncTestConsoleApp;
+
+var cancellationToken = new CancellationToken();
+
+var stopwatch = Stopwatch.StartNew();
+stopwatch.Start();
+await AsyncClass.WillSitAndWaitForSomeTime(5, cancellationToken);
+await AsyncClass.WillSitAndWaitForSomeTime(2, cancellationToken);
+stopwatch.Stop();
+var fakeAsyncTimeSpan = stopwatch.Elapsed;
+
+stopwatch.Restart();
+var task1 = AsyncClass.WillSitAndWaitForSomeTime(5, cancellationToken);
+var task2 = AsyncClass.WillSitAndWaitForSomeTime(2, cancellationToken);
+await Task.WhenAll(task1, task2);
+stopwatch.Stop();
+var trulyAsyncTimeSpan = stopwatch.Elapsed;
+
+Console.WriteLine("Here comes results.");
+Console.WriteLine($"FakeAsync methods took {fakeAsyncTimeSpan.TotalSeconds} seconds");
+Console.WriteLine($"TrulyAsync methods took {trulyAsyncTimeSpan.TotalSeconds} seconds");
+Console.WriteLine("That's all folks!");
